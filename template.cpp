f349c25bc072de8c3891c13d3b959778fb18f808
MatheusViniciusV <101892449+MatheusViniciusV@users.noreply.github.com>
2023-12-21 16:52:05 -0300

    Template criado

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastios std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define int long long 
+#define endl "\n"
+#define loop(i,a,n) for(int i=a; i < n; i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define dbg(x) cout << #x << " = " << x << endl
+
+int32_t main(){ fastios
+
+
+    return 0;
+}
