ef45f85f996970d2d0f66b4f2c53fec5ba9babc4
MatheusViniciusV <101892449+MatheusViniciusV@users.noreply.github.com>
2024-01-06 20:39:01 -0300

    Algumas mudanças

--- template.cpp
+++ template.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 
 #define fastios std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#define int long long 
-#define endl "\n"
-#define loop(i,a,n) for(int i=a; i < n; i++)
-#define pb push_back
-#define ff first
-#define ss second
 #define dbg(x) cout << #x << " = " << x << endl
+#define endl "\n"
+#define pb push_back
+#define f first
+#define s second
 
-int32_t main(){ fastios
+int main()
+{
+    fastios
 
+    
 
     return 0;
 }
