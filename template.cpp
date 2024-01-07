3397fb8dad2f2849bfff039a59c72bbd2478ac76
MatheusViniciusV <101892449+MatheusViniciusV@users.noreply.github.com>
2024-01-06 22:02:48 -0300

    Mais mudanças

--- template.cpp
+++ template.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #define fastios std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 #define dbg(x) cout << #x << " = " << x << endl
 #define endl "\n"
+#define ll long long 
 #define pb push_back
 #define f first
 #define s second
